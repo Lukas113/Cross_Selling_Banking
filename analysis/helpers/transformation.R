40eb670e0f2a217491f4aeefe6ee6fbcfe457a0f
lukas <lukas.gehrig@students.fhnw.ch>
2019-11-20 18:18:32 +0100

    Einkommensschätzer aktualisiert

--- analysis/helpers/transformation.R
+++ analysis/helpers/transformation.R
@@ -7,7 +7,18 @@ corr_trans <- function(trans_frame){
   #' - transforms type('VYBER') to type('PRIJEM')
   
   trans_frame <- trans_frame %>%
-    mutate(type = replace(type, type == 'VYBER', 'PRIJEM'))
+    mutate(type = replace(type, type == 'VYBER', 'VYDAJ'))
   
   return(trans_frame)
+}
+
+round2 = function(x, n) {
+  #' @description rounds mathematically correct, e.g. rounds 0.5 up instead of down like base::round
+  
+  posneg = sign(x)
+  z = abs(x)*10^n
+  z = z + 0.5
+  z = trunc(z)
+  z = z/10^n
+  return(z*posneg)
 }

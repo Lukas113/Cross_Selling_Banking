7a02b0211b619c07225766fffabf7f71593947c1
Simon_Staehli <simon.staehli@students.fhnw.ch>
2019-11-29 13:49:49 +0100

    Distributions of ages plotted and additional function generated

--- analysis/helpers/prepareData.R
+++ analysis/helpers/prepareData.R
@@ -34,6 +34,39 @@ get_count_by_gender <- function() {
   return(data)
 }
 
+get_districts <- function(){
+  "SELECT a1 as district_code, a2 as district_name,
+  a3 as region, a4 as no_of_inhabitants, a5 as municipalities_u499,
+  a6 as municipalties_500_1999, a7 as municipalties_2000_9999, a8 as municipalties_o9999,
+  a9 as no_of_cities, a10 as ratio_urban_inhabitants, a11 as avg_salary,
+  a12 as unemploymt_rate95, a13 as unemploymt_rate96, a14 as no_enterpreneurs_per_1000,
+  a15 as commited_crimes95, a16 as commited_crimes96 
+  from district" 
+}
+
+get_all_clients_join_region_ctype <- function(){
+  data <- dbGetQuery(con,"SELECT  client.client_id,
+  CASE WHEN MOD(client.birth_number / 100, 100) > 50 THEN 
+  'f'
+  ELSE
+  'm'
+  END as gender,
+  CASE WHEN MOD(client.birth_number / 100, 100) > 50 THEN
+  TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD')
+  ELSE
+  TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD')
+  END as birthdate,
+  EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(client.birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(client.birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(client.birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) as age,
+  card.type, district.a3 AS region
+  FROM client
+  JOIN disp ON client.client_id = disp.client_id
+  LEFT JOIN card ON disp.disp_id = card.disp_id
+  JOIN district ON client.district_id = district.a1
+  ORDER BY client_id") 
+  return(data)
+}
+
+
 get_all_clients <- function() {
   data = dbGetQuery(con, "SELECT *,
 	CASE WHEN MOD(birth_number / 100, 100) > 50 THEN 
@@ -47,6 +80,6 @@ get_all_clients <- function() {
 		TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD')
 	END as birthdate,
 	EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) as age
-  FROM client LIMIT 10")
+  FROM client")
   return(data)
 }

13a552fa78bcd59c2b9e299fb8bd7192c07f01db
Lukas Märki <maerki@luukas.ch>
2019-12-03 18:56:18 +0100

    kartenzeugs

--- analysis/helpers/prepareData.R
+++ analysis/helpers/prepareData.R
@@ -12,6 +12,7 @@ get_all_card_count <- function() {
 
 get_card_count_by_card_type <- function() {
   data = dbGetQuery(con, "SELECT type, count(type) FROM card group by type")
+  data$type <- factor(data$type, levels = c("classic", "junior", "gold"))
   return(data)
 }
 
@@ -34,7 +35,52 @@ get_count_by_gender <- function() {
   return(data)
 }
 
-get_districts <- function(){
+get_client_age_by_issued_date <- function() {
+  data = dbGetQuery(con, "select card.card_id, card.type, card.issued, client.*,
+CASE WHEN MOD(client.birth_number / 100, 100) > 50 THEN
+  	TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD')
+  ELSE
+  	TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD')
+  END as birthdate,
+  EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) as current_age,
+  EXTRACT(YEAR FROM AGE(card.issued, CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) as age_on_card_issue
+from card
+join disp on card.disp_id = disp.disp_id
+join client on disp.client_id = client.client_id")
+  return(data)
+}
+
+get_clients_without_credit_card_by_age_range <- function(from_age = 0, to_age = 25) {
+  data = dbGetQuery(con, sprintf("select disp.disp_id, disp.account_id, disp.client_id,
+  EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) as age
+  from account
+  
+  join disp on account.account_id = disp.account_id
+  join client on disp.client_id = client.client_id
+  
+  where not exists(
+    select * from disp where account.account_id = disp.account_id and exists (
+      	select * from card where disp.disp_id = card.disp_id
+      )
+    )
+    
+  and disp.type = 'OWNER'
+  and EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) > %s
+  and EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) < %s
+  order by account.account_id", from_age, to_age))
+  return(data)
+}
+
+get_count_by_client_age <- function() {
+  data = dbGetQuery(con, "SELECT
+	EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)) as age,
+	count(EXTRACT(YEAR FROM AGE('1998-12-31', CASE WHEN MOD(birth_number / 100, 100) > 50 THEN TO_DATE(CONCAT('19', CAST(birth_number-5000 AS VARCHAR(6))), 'YYYYMMDD') ELSE TO_DATE(CONCAT('19', CAST(birth_number AS VARCHAR(6))), 'YYYYMMDD') END)))
+  FROM client
+  group by age")
+  return(data)
+}
+
+get_districts <- function() {
   "SELECT a1 as district_code, a2 as district_name,
   a3 as region, a4 as no_of_inhabitants, a5 as municipalities_u499,
   a6 as municipalties_500_1999, a7 as municipalties_2000_9999, a8 as municipalties_o9999,
@@ -44,7 +90,7 @@ get_districts <- function(){
   from district" 
 }
 
-get_all_clients_join_region_ctype <- function(){
+get_all_clients_join_region_ctype <- function() {
   data <- dbGetQuery(con,"SELECT  client.client_id,
   CASE WHEN MOD(client.birth_number / 100, 100) > 50 THEN 
   'f'
